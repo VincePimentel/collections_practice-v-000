d0c2a07445f5894908e34264c0a62443b522a484
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 21:50:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.each do |i|
-    array[1] = array[2]
-    array[2] = array[1]
-  end
+  #array.each do |i|
+    array << array[2]
+    array << array[1]
+  #end
 end
